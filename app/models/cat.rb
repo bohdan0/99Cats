523f9eddf9c1a79310c8c4430b36d0ce34b20dfb
Bohdan Nakonechnyi <bnakonechnuy0@gmail.com>
2016-12-06 10:16:02 -0800

    Create cat model and table

---
+++ app/models/cat.rb
@@ -0,0 +1,8 @@
+class Cat < ActiveRecord::Base
+  validates :birth_date, :color, :sex, :name, :description, presence: true
+  validates :color, inclusion: { in: %w(black white red green),
+    message: "%{value} is not a valid color" }
+  validates :sex, inclusion: { in: %w(M F),
+    message: "%{value} is not a valid sex"}
+
+end
